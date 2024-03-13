b91c2933f7665123a9b5fba9b0929984b5b9cb1f
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2024-03-13 00:56:58 -0700

    formatted companies

--- client/src/pages/Companies.jsx
+++ client/src/pages/Companies.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Heading, SimpleGrid, Button } from '@chakra-ui/react'
 
 const Companies = () => {
     const [companies, setCompanies] = useState([])
@@ -8,17 +10,38 @@ const Companies = () => {
     useEffect(()=>{
         const fetchAllBooks = async ()=> {
             try {
-                const res = await axios.get(URL + "/companies")
-                console.log(res)
+                const res = await axios.get(URL + "/companies");
+                console.log(res.data);
+                setCompanies(res.data);
             } catch(err){
-                console.log(err)
+                console.log(err);
             }
         }
         fetchAllBooks();
-    }, [])
+    }, []);
 
     return (
-        <div>Companies</div>
+        <div className='company'>
+            <Heading size='4xl'>Companies</Heading>
+            <br/>
+            <Button>Add new company</Button>
+            <br/>
+            <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(300px, 1fr))'>
+                {companies.map(company=>(
+                    <Card variant='outline'>
+                        <CardHeader>
+                            <Heading> {company.COMPANY_NAME}</Heading>
+                        </CardHeader>
+                        <CardBody>
+                            Started in: {company.START_YEAR}
+                            <br/>
+                            CEO: {company.CEO_LNAME} {company.CEO_FNAME}
+                        
+                        </CardBody>
+                    </Card>
+                ))}
+            </SimpleGrid>
+        </div>
     )
 }
 
