8ead627dd3b747010cd136399f4faa856513ce25
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2024-03-13 00:56:41 -0700

    formatted groups page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
 import Companies from "./pages/Companies";
 import Add from "./pages/Add";
 import Update from "./pages/Update";
+import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" eleme
-        <Route path="/companies" element={<Companies />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/update" element={<Update />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
-
+    <ChakraProvider>
+      <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/companies" element={<Companies />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+      </div>
+    </ChakraProvider>
   );
 }
 export default App;
---
+++ client/src/pages/Groups.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Heading, SimpleGrid, Button } from '@chakra-ui/react'
+
+const Groups = () => {
+    const [groups, setGroups] = useState([])
+    const URL = "http://localhost:8800"
+    
+    useEffect(()=>{
+        const fetchAllBooks = async ()=> {
+            try {
+                const res = await axios.get(URL + "/groups");
+                console.log(res.data);
+                setCompanies(res.data);
+            } catch(err){
+                console.log(err);
+            }
+        }
+        fetchAllBooks();
+    }, []);
+
+    return (
+        <div className='company'>
+            <Heading size='4xl'>Groups</Heading>
+            <br/>
+            <Button>Add new group</Button>
+            <br/>
+            <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(300px, 1fr))'>
+                {groups.map(group=>(
+                    <Card variant='outline'>
+                        <CardHeader>
+                            <Heading> {group.G_NAME}</Heading>
+                        </CardHeader>
+                        <CardBody>
+                            Generation: {group.G_GEN}
+                            <br/>
+                            Company: {group.G_COMPANY}
+                            <br/>
+                            Debut Year: {group.G_DEBUT}        
+                        </CardBody>
+                    </Card>
+                ))}
+            </SimpleGrid>
+        </div>
+    )
+}
+
+export default Groups;
